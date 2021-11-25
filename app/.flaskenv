d88ee06941b151ad8a823db9d14fbee43257dd81
Maestro28 <bohdan.rybak28@gmail.com>
2021-11-25 14:05:16 +0200

    part 7 complete

--- app/.flaskenv
+++ app/.flaskenv
@@ -1,1 +1,5 @@
 FLASK_APP=microblog.py
+FLASK_DEBAG=1
+FLASK_ENV=“development"
+env:FLASK_ENV = "development"
+env:FLASK_ENV="development"
--- config.py
+++ config.py
@@ -7,5 +7,9 @@ class Config(object):
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-"""from microblog import app
-print(app.config["SECRET_KEY"])"""
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['data2maestro28@gmail.com']
