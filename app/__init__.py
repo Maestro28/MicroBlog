49deb0e58d7f45edb813677601ca412b971d8663
Maestro28 <bohdan.rybak28@gmail.com>
2021-11-28 07:58:02 +0200

    part X complete! (email mesages didn't work because it was without any server)

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +16,8 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+
+mail = Mail(app)
 
 #app.debug = True #dont work
 if not app.debug:
